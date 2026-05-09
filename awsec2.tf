254f21b517c6bf36f0a86dcbcf533a500e3c0728
szeevi <66994877+szeevi@users.noreply.github.com>
2026-05-09 23:20:15 +0300

    Create awsec2.tf

---
+++ awsec2.tf
@@ -0,0 +1,15 @@
+awsec2.tf  instance.yml  terraform.tfstate  terraform.tfstate.backup
+sz@DESKTOP-9UQE46U:~/terraform$ cat awsec2.tf
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "terraform_demo" {
+ami = "ami-0eba005a3e64054e1"
+instance_type = "t3.micro"
+availability_zone = "us-east-1c"
+vpc_security_group_ids = ["sg-00ef03cc35ddaa39c"]
+
+tags = {
+    Name = "TerraformOS"
+}
